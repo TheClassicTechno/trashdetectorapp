04f920c009ae8a5f1810b491337b0dc474ab6bc8
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2022-10-23 18:48:10 -0400

    Update debridetec.py

--- debridetec.py
+++ debridetec.py
@@ -353,10 +353,7 @@ if selection == 'CONTACT US':
     st.markdown('<p class="font">Contact PlantDoc Creators</p>', unsafe_allow_html=True)
    
 
-    st.markdown(""" <style> .font2 {
-    font-size:30px ; font-weight: 600; color: #0a0302;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font2">About the Creators</p>', unsafe_allow_html=True)
+    
 
     st.markdown(""" <style> .font3 {
     font-size:20px ; color: #fffafa;} 
@@ -368,36 +365,21 @@ if selection == 'CONTACT US':
         st.write("[Click to send us an email](mailto:juliah6169@gmail.com)") #displayed when the button is clicked
 
     
-    
+         
     st.markdown(""" <style> .font2 {
     font-size:30px ; font-weight: 600; color: #0a0302;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font2">Mission</p>', unsafe_allow_html=True)
-
+    st.markdown('<p class="font2">Other Information</p>', unsafe_allow_html=True)
+    
     st.markdown(""" <style> .font3 {
-    font-size:20px ;  color: #fffafa;} 
+    font-size:20px ; color: #0a0302;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font3">Due to the high usage of pesticides, many plant diseases have become resilient and more common. In addition, farmers, especially those living in isolated or rural parts of the world, may not know what or if a disease has affected their plant. Therefore it is essential to figure out what diseases are affecting plants so consumers do not get sick. So the goal of PlantDoc is to provide the farmers and gardeners an opportunity to check the conditions of each and every plant they tend to. PlantDoc aims to make this checking process simpler and more convenient by utilizing AI & machine learning.</p>', unsafe_allow_html=True)
-    
-    st.markdown(""" <style> .font2 {
-    font-size:30px ; font-weight: 600; color: #0a0302;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font2">How PlantDoc was Built</p>', unsafe_allow_html=True)
-
-    st.markdown(""" <style> .font3 {
-    font-size:20px ; color: #fffafa;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font3">PlantDoc has two parts: the AI model and web app. The AI model is built using the TensorFlow framework in the Python Language while the web app is built using Streamlit using HTMl/CSS formatting. We trained the model in Google Colab on a dataset consisting of 15 types of plant conditions sourced from the PlantVillage dataset on Kaggle and deployed the model into the web app with Streamlit.</p>', unsafe_allow_html=True)
-    
-    st.markdown(""" <style> .font2 {
-    font-size:30px ; font-weight: 600; color: #0a0302;} 
-    </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font2">Future of PlantDoc</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3">Streamlit Website Developed By Julia and Justin Huang. Established since Summer/Fall 2022.</p>', unsafe_allow_html=True)
 
     st.markdown(""" <style> .font3 {
     font-size:20px ; color: #0a0302;} 
     </style> """, unsafe_allow_html=True)
-    st.markdown('<p class="font3">The accuracy of our CNN model is currently 92%, but we plan to improve the accuracy of the AI model even more. We also plan to partner with agricultural businesses so we can test out the app with farmers.</p>', unsafe_allow_html=True)
+    st.markdown('<p class="font3"> Copyright © 2022. United States.</p>', unsafe_allow_html=True)
     
     
      
