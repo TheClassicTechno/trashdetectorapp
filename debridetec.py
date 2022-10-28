8a20f8c510d077cd7e60627287ea239ceaf0a604
TheClassicTechno <92133615+TheClassicTechno@users.noreply.github.com>
2022-10-28 18:06:02 -0400

    Update debridetec.py

--- debridetec.py
+++ debridetec.py
@@ -150,7 +150,7 @@ if selection == 'ABOUT OUR APP':
     font-size:50px ; font-weight: 800; color: #ff958a; background-color: #fffafa;} 
     </style> """, unsafe_allow_html=True)
     st.markdown('<p class="font">About DebriDetec</p>', unsafe_allow_html=True)
-   (bottom_image, width=400)
+
     st.image("debrideteclogo.png", width = 300)
     st.markdown(""" <style> .font2 {
     font-size:30px ; font-weight: 600; color: #0a0302;} 
